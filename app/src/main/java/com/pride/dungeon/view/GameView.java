19534f974b0a1c0b6474753aac38a8e11b094c02
Administrator <administrator@Administrators-MacBook-Pro.local>
2016-06-27 19:21:08 +0300

    Create class GameView

--- app/src/main/java/com/pride/dungeon/view/GameView.java
+++ app/src/main/java/com/pride/dungeon/view/GameView.java
@@ -1,7 +1,27 @@
 package com.pride.dungeon.view;
 
-/**
- * Created by administrator on 6/27/16.
- */
-public class GameView {
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
 }
